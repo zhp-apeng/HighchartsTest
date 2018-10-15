3ca5774d156d86d0060c256220e3ea542d20672a
Zhong Peng(WB) <wb-pzhong1@yfpo.com>
2018-10-15 16:46:03 +0800

    update

--- HighchartsTest/Controllers/HomeController.cs
+++ HighchartsTest/Controllers/HomeController.cs
@@ -12,6 +12,37 @@ namespace HighchartsTest.Controllers
         {
             ViewBag.Message = "修改此模板以快速启动你的 ASP.NET MVC 应用程序。";
 
+
+            //后台取数据
+            //List<XModel> XModelList = new List<XModel>();
+            //for (int i = 0; i < dataArray[6].Length; i++)
+            //{
+            //    XModel Xmodel = new XModel();
+            //    Xmodel.XID = i + 1;
+            //    Xmodel.XValue = Math.Round(Convert.ToDecimal(dataArray[6][i]) / 100, 2);
+            //    XModelList.Add(Xmodel);
+            //}
+            //ViewBag.XModelList = XModelList;
+
+
+
+
+            //前台x轴赋值
+            //@{
+            //var RModelList = ViewBag.RModelList as List<BSNSManage.Models.RModel>;
+            //var RValue = RModelList.Select(p => p.RValue);
+            //}
+            //X轴值
+            //series: [{
+            //    name: '平均值',
+            //    //data: [109, 109, 111, 112.3, 109.4, 108.8, 108.8, 110.1, 109, 110.9, 109, 108]
+            //    data: [@string.Join(",", XValue)]
+
+
+            //}
+
+
+
             return View();
         }
 
